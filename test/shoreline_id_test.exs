e482ccc83a6aac7476c95fa028b3e9e7e6f8f927
JLarky <jlarky@gmail.com>
2020-03-15 18:36:33 -0700

    add failing test: overflown id has right ts only on first call, then it resets back to wrong ts

--- test/shoreline_id_test.exs
+++ test/shoreline_id_test.exs
@@ -8,7 +8,7 @@ defmodule GlobalIdTest do
     assert GlobalId.get_id() == 6_645_053_045_335_916_545
   end
 
-  test "calling get_id too many times in one millisecond will overflow counter so ts will increase instead" do
+  test "BUG: calling get_id too many times in one millisecond will overflow counter so ts will increase instead" do
     {:ok, _pid} = GlobalId.start_link()
     id = GlobalId.get_id()
     for _ <- 1..2047, do: GlobalId.get_id()
@@ -16,6 +16,8 @@ defmodule GlobalIdTest do
     overflow_id = GlobalId.get_id()
     assert overflow_id !== id
     assert {1_584_304_105_124, 1024, 0} === GlobalId.inspect_id(overflow_id)
+    overflow_id = GlobalId.get_id()
+    assert {1_584_304_105_124, 1024, 1} === GlobalId.inspect_id(overflow_id)
   end
 
   test "BUG: counter overflow is not safe on GenServer restart" do
